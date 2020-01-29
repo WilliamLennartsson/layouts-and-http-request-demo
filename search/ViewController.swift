1698a5b4ea4efed0b231d49a9f3f6b7978ed96e1
William Lennartsson <william.j.lennartsson@gmail.com>
2020-01-29 09:39:11 +0100

    Initial Commit

---
+++ search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  search
+//
+//  Created by William Lennartsson on 2020-01-29.
+//  Copyright © 2020 William Lennartsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
